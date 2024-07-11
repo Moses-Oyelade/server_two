5419bb65eab1787de83c4825274dd0d89b1c9de2
Moses-Oyelade <oyelademoses@gmail.com>
2024-07-11 19:32:29 +0100

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from extensions import db, jwt
 from auth import auth_bp
 from api import api_bp
@@ -16,9 +16,21 @@ def create_app():
     
     # register blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
-    
-    # Routing blueprints
     app.register_blueprint(api_bp, url_prefix='/api')
     
+    
+    # jwt error handlers
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header,jwt_data):
+        return jsonify({"message":"Token has expired", "error":"token_expired"})
+    
+    
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return jsonify({"message":"Signature verification failed", "error":"invalid_token"})
+    
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return jsonify({"message":"Request does not contain valid token", "error":"authorization_header"})
     
     return app
